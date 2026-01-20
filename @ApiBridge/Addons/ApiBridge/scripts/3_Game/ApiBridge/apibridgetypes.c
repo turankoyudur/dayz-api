f3f5db9123e1f293e43d4fd19615cb9cc9a369eb
turankoyudur <32541468+turankoyudur@users.noreply.github.com>
2026-01-20 19:05:31 +0300

    Consolidate state models in ApiBridgeModels

--- @ApiBridge/Addons/ApiBridge/scripts/3_Game/ApiBridge/apibridgetypes.c
+++ @ApiBridge/Addons/ApiBridge/scripts/3_Game/ApiBridge/apibridgetypes.c
@@ -51,33 +51,4 @@ class ApiBridgeResponseBatch
 }
 
 // ----- State -----
-class ApiBridgeItemState
-{
-    string type;
-    float quantity;
-    float health;
-    string location; // cargo / attachment / hands / unknown
-}
-
-class ApiBridgePlayerState
-{
-    string uid;
-    string name;
-    float x;
-    float y;
-    float z;
-
-    float health;
-    float blood;
-    float water;
-    float energy;
-
-    ref array<ref ApiBridgeItemState> inventory;
-}
-
-class ApiBridgeState
-{
-    int ts;
-    int playerCount;
-    ref array<ref ApiBridgePlayerState> players;
-}
+// State models are defined in ApiBridgeModels.c to avoid duplicate declarations.
